b96c6a4f31e59aa42c88b88deb84d5152aa6ac65
VladokAS <vladok.ru139@gmail.com>
2017-05-22 23:36:01 +0300

    Some animations
    Rabbit is able to jump, run, and stand calm. Was created Death ZOne.
    Also, Parrallax was added

--- Assets/Content/Scripts/Rabbit.cs
+++ Assets/Content/Scripts/Rabbit.cs
@@ -7,28 +7,88 @@ public class Rabbit : MonoBehaviour {
 	Rigidbody2D myBody = null;
 
 	// Use this for initialization
-	void Start () {
-		myBody = this.GetComponent<Rigidbody2D> ();	
-	}
+	void Start ()
+    {
+        LevelController.current.setStartPosition(transform.position);
+        myBody = this.GetComponent<Rigidbody2D> ();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    bool isGrounded = false;
+    bool JumpActive = false;
+    float JumpTime = 0f;
+    public float MaxJumpTime = 2f;
+    public float JumpSpeed = 2f;
 
-	void FixedUpdate () {
-		//[-1, 1]
-		float value = Input.GetAxis ("Horizontal");
-		if (Mathf.Abs (value) > 0) {
-			Vector2 vel = myBody.velocity;
+    void FixedUpdate () {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            this.JumpActive = true;
+        }
+        if (this.JumpActive)
+        {
+            //Якщо кнопку ще тримають
+            if (Input.GetButton("Jump"))
+            {
+                this.JumpTime += Time.deltaTime;
+                if (this.JumpTime < this.MaxJumpTime)
+                {
+                    Vector2 vel = myBody.velocity;
+                    vel.y = JumpSpeed * (1.0f - JumpTime / MaxJumpTime);
+                    myBody.velocity = vel;
+                }
+            }
+            else
+            {
+                this.JumpActive = false;
+                this.JumpTime = 0;
+            }
+        }
+        //[-1, 1]
+        float value = Input.GetAxis ("Horizontal");
+
+        Animator animator = GetComponent<Animator>();
+        if (Mathf.Abs (value) > 0) {
+            animator.SetBool("Run", true);
+            Vector2 vel = myBody.velocity;
 			vel.x = value * speed;
 			myBody.velocity = vel;
-		}
-		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        }
+        else animator.SetBool("Run", false);
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
 		if(value < 0) {
 			sr.flipX = true;
 		} else if(value > 0) {
 			sr.flipX = false;
 		}
-	}
+
+        Vector3 from = transform.position + Vector3.up * 0.3f;
+        Vector3 to = transform.position + Vector3.down * 0.1f;
+        int layer_id = 1 << LayerMask.NameToLayer("Ground");
+        //Перевіряємо чи проходить лінія через Collider з шаром Ground
+        RaycastHit2D hit = Physics2D.Linecast(from, to, layer_id);
+        if (hit)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+        //Намалювати лінію (для розробника)
+        Debug.DrawLine(from, to, Color.red);
+        
+        if (this.isGrounded)
+        {
+            animator.SetBool("Jump", false);
+        }
+        else
+        {
+            animator.SetBool("Jump", true);
+        }
+    }
 }
